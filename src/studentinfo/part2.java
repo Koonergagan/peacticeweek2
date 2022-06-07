10dceb372fda7bad1dab7f9407ac63615eb8245c
gagan <koonerga@sheridancollege.ca>
2022-06-07 12:53:36 -0400

    infocourses addedd

---
+++ src/studentinfo/part2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package studentinfo;
+
+/**
+ *
+ * @author Gagandeep kooner
+ */
+public class part2 {
+
+    private int numOfcourses;
+
+    /**
+     * @return the numOfcourses
+     */
+    public int getNumOfcourses() {
+        return numOfcourses;
+    }
+
+    /**
+     * @param numOfcourses the numOfcourses to set
+     */
+    public void setNumOfcourses(int numOfcourses) {
+        this.numOfcourses = numOfcourses;
+    }
+    
+    
+}
